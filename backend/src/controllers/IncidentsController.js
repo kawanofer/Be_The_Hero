4410bea4fe5b41ec5a714788d441d3dd23dc8a8f
Fernando Kawano <fernando.kawano@vexpro.com.br>
2020-03-28 19:55:37 -0300

    frontend - backend - OK

---
+++ backend/src/controllers/IncidentsController.js
@@ -0,0 +1,64 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async index(request, response) {
+        const { page = 1 } = request.query;
+
+        const [count] = await connection('incidents')
+            .count();
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select([
+                'incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf']);
+
+        response.header('X-Total-Count', count['count(*)']);
+
+        return response.json(incidents);
+    },
+
+    async create(req, res) {
+        const { title, description, value } = req.body;
+        console.log("-> ", req.body);
+
+        const ong_id = req.headers.authorization;
+        console.log("-> ", ong_id);
+
+        if(!req.body)
+        {
+            return res.json("invalido");
+        }
+        const [id] = await connection('incidents').insert({
+            title,
+            description,
+            value,
+            ong_id,
+        });
+
+        return res.json({ id });
+    },
+
+    async delete(request, response) {
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+
+        const incident = await connection('incidents')
+            .where('id', id).select('ong_id')
+            .first();
+
+        if (incident.ong_id != ong_id) {
+            return response.status(401).
+                json({ error: 'Operation not permitted.' });
+        }
+        await connection('incidents').where('id', id).delete();
+
+        return response.status(204).send();
+    }
+};
